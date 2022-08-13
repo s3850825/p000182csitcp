5113d770c9baa1b190534f49abae220027df9e57
s3850825 <s3850825@student.rmit.edu>
2022-08-13 21:39:48 +1000

    RSA key generation of CA has been implemented

--- certificate_authority/scripts/test.py
+++ certificate_authority/scripts/test.py
@@ -1,8 +1,11 @@
 from scripts.deploy import deploy_certification, create_certificate, get_certificate
 from scripts.verification import verify_signature
+from scripts.keyGeneration import generate_CA_key_pairs
 
 
 def main():
+    CA_public_key, CA_private_key = generate_CA_key_pairs()
+    print(CA_public_key, CA_private_key)
     deploy_certification()
     create_certificate("Alice", 123)
     get_certificate()
