8143a2ee7a94ff5760acaf97588e1abedf843e50
s3850825 <s3850825@student.rmit.edu>
2022-08-26 00:14:33 +1000

    Implemented how to sign and verify using RSA key pairs but still need to figure out how to convert string signature from Solidity to bytes in python

--- certificate_authority/scripts/test.py
+++ certificate_authority/scripts/test.py
@@ -2,21 +2,14 @@ from scripts.deploy import deploy_certification, create_certificate, get_certifi
 from scripts.verification import verify_signature
 from scripts.keyGeneration import generate_CA_key_pairs
 from Crypto.Hash import SHA256
-from Crypto.Signature import PKCS1_v1_5
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
+import binascii
 
 def main():
     KeyPair = generate_CA_key_pairs()
 
-    # publickey = "1234"
-    # digest = SHA256.new()
-    # digest.update(publickey.encode('utf-8'))
-
-    # signer = PKCS1_v1_5.new(keyPair)
-    # signature = signer.sign(digest)
-
-    # print(signature)
-
     deploy_certification()
-    create_certificate("Alice", 123)
+    user_publickey = 123
+    create_certificate("Alice", user_publickey, KeyPair)
     get_certificate(KeyPair)
-    verify_signature(123, 30, KeyPair)
+    verify_signature(user_publickey, KeyPair)
