d70052c4e37007b6e6dcb830960e224c8cf4ee83
s3850825 <s3850825@student.rmit.edu.au>
2022-09-21 20:30:42 +1000

    test.py in certificate_authority folder has been modified to test frontend-ui

--- certificate_authority/scripts/test.py
+++ certificate_authority/scripts/test.py
@@ -4,12 +4,17 @@ from scripts.keyGeneration import generate_CA_key_pairs
 from Crypto.Hash import SHA256
 from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 import binascii
+from interfaces.frontend.main import frontend_main
 
 def main():
     KeyPair = generate_CA_key_pairs()
 
-    deploy_certification()
-    user_publickey = 123
-    create_certificate("Alice", user_publickey, KeyPair)
-    get_certificate(KeyPair)
-    verify_signature(user_publickey, KeyPair)
+    # open frontend UI
+    frontend_main()
+
+    # we need to run these in frontend UI
+    # deploy_certification()
+    # user_publickey = 123
+    # create_certificate("Alice", user_publickey, KeyPair)
+    # get_certificate(KeyPair)
+    # verify_signature(user_publickey, KeyPair)
