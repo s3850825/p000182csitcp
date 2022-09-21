79add168ec83064588503943643518aaeadbce54
s3850825 <s3850825@student.rmit.edu>
2022-09-21 20:23:00 +1000

    merged connect-DB and frontend-UI branch

--- certificate_authority/scripts/test.py
+++ certificate_authority/scripts/test.py
@@ -4,12 +4,18 @@ from scripts.keyGeneration import generate_CA_key_pairs
 from Crypto.Hash import SHA256
 from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 import binascii
+from interfaces.frontend.main import frontend_main
 
 def main():
     KeyPair = generate_CA_key_pairs()
+    
+    # open frontend UI
+    frontend_main()
 
-    deploy_certification()
-    user_publickey = 123
-    create_certificate("Alice", user_publickey, KeyPair)
-    get_certificate(KeyPair)
-    verify_signature(user_publickey, KeyPair)
+    # we need to run these in frontend UI
+    
+    # deploy_certification()
+    # user_publickey = 123
+    # create_certificate("Alice", user_publickey, KeyPair)
+    # get_certificate(KeyPair)
+    # verify_signature(user_publickey, KeyPair)
