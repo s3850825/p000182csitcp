74823bacd7a63d8b1d68950950b74f98ce9efae6
s3850825 <s3850825@student.rmit.edu.au>
2022-10-08 22:51:26 +1100

    Oracle DB has been added

---
+++ certificate_authority/db/databaseOracle.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0b0f0fdf7fa27810bf194059df37fdda84ec9fd8cb5b995c6e4f7735fb51e672
+size 4297
--- certificate_authority/scripts/keyGeneration.py
+++ certificate_authority/scripts/keyGeneration.py
@@ -1,17 +1,3 @@
-from Crypto.PublicKey import RSA
-
-def generate_RSA_key_pairs(studentName):
-    # Use RSA to generate CA's key pairs
-    KeyPair = RSA.generate(bits=1024)
-
-    privKey = KeyPair.exportKey('PEM')
-    pubKey = KeyPair.publickey().exportKey('PEM')
-
-    #save PEM key into the file
-    with open(studentName + '_private.pem', 'wb') as file:
-        file.write(privKey)
-
-    with open(studentName+ '_public.pem', 'wb') as file:
-        file.write(pubKey)
-
-    return privKey, pubKey
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e41b03d06726c8058bccfa76699dc637fc6dcba639bbf0a2d510e0842224f7d
+size 476
