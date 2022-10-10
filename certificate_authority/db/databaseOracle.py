a5e20639bd4411870939f655ef05908f1eb08735
s3850825 <s3850825@student.rmit.edu.au>
2022-10-10 14:04:24 +1100

    Used relative path for both windows and mac users

--- certificate_authority/db/databaseOracle.py
+++ certificate_authority/db/databaseOracle.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b0f0fdf7fa27810bf194059df37fdda84ec9fd8cb5b995c6e4f7735fb51e672
-size 4297
+oid sha256:21a0e1eab1da761779d5f270131b16bb0317b7bce79577a8b6d479ce4259710e
+size 4273
